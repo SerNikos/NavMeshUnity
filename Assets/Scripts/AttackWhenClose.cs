6e4b3568a36d54c3f9bd8906b8851520478b4b07
SerNikos <78097143+SerNikos@users.noreply.github.com>
2021-08-30 15:16:56 +0300

    Initial commit
    I did my first clone project

---
+++ Assets/Scripts/AttackWhenClose.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackWhenClose : MonoBehaviour
+{
+
+    private Transform enemy;
+    private float dist;
+    
+    public float howClose = 10;
+
+
+    public GameObject target;
+    public Transform spawnPos;
+    public GameObject bullet;
+    private float SpeedBullet= 10f;
+
+    public bool canFire= true;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(enemy.position, transform.position);
+
+        if(dist<= howClose)
+        {
+            transform.LookAt(enemy);
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+
+            StartCoroutine( WaitForBullets());
+
+           
+
+
+        }
+    }
+
+    public void Shoot()
+    {
+        if (canFire)
+        {
+            GameObject Spawner = Instantiate(bullet, spawnPos.position, spawnPos.transform.rotation);
+            Spawner.GetComponent<Rigidbody>().velocity += transform.forward * SpeedBullet;
+        }
+        
+    }
+
+  IEnumerator WaitForBullets()
+    {
+        
+        print("NIKOS"); 
+        Shoot();
+        canFire = false;
+        
+   
+        yield return new WaitForSeconds(5);
+        canFire = true;
+        WaitForBullets();
+    }
+}
---
+++ Assets/Scripts/BullterDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullterDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision col)
+    {
+       if (col.gameObject.tag == "Bullet")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
