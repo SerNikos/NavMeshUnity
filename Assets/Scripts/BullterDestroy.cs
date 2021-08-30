a02104e1f27523bf5e470ca81e7eb1261ec59743
Kyriakos <kiriakostriadafillidis@gmail.com>
2021-08-30 15:24:19 +0300

    Added a comment

--- Assets/Scripts/BullterDestroy.cs
+++ Assets/Scripts/BullterDestroy.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BullterDestroy : MonoBehaviour
 {
+    //never use tags for anything its a bad practice.
+
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision col)
     {
-       if (col.gameObject.tag == "Bullet")
+        if (col.gameObject.tag == "Bullet")
         {
             Destroy(col.gameObject);
         }
